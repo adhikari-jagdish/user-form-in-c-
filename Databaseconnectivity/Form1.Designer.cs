a94acc51bda0af870488f5f7c86d239d845fc9f1
adhikari-jagdish <grango153@gmail.com>
2017-04-01 01:18:02 +0530

    first upload of the form app

---
+++ Databaseconnectivity/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Databaseconnectivity
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.id = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.age = new System.Windows.Forms.TextBox();
+            this.show = new System.Windows.Forms.Button();
+            this.insert = new System.Windows.Forms.Button();
+            this.update = new System.Windows.Forms.Button();
+            this.view = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.delete = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // id
+            // 
+            this.id.Location = new System.Drawing.Point(165, 55);
+            this.id.Name = "id";
+            this.id.Size = new System.Drawing.Size(183, 20);
+            this.id.TabIndex = 0;
+            this.id.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(91, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Id";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(91, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Name";
+            // 
+            // name
+            // 
+            this.name.Location = new System.Drawing.Point(165, 102);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(183, 20);
+            this.name.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(91, 160);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Age";
+            // 
+            // age
+            // 
+            this.age.Location = new System.Drawing.Point(165, 153);
+            this.age.Name = "age";
+            this.age.Size = new System.Drawing.Size(183, 20);
+            this.age.TabIndex = 5;
+            // 
+            // show
+            // 
+            this.show.Location = new System.Drawing.Point(386, 51);
+            this.show.Name = "show";
+            this.show.Size = new System.Drawing.Size(75, 23);
+            this.show.TabIndex = 6;
+            this.show.Text = "Get";
+            this.show.UseVisualStyleBackColor = true;
+            this.show.Click += new System.EventHandler(this.show_Click);
+            // 
+            // insert
+            // 
+            this.insert.Location = new System.Drawing.Point(60, 207);
+            this.insert.Name = "insert";
+            this.insert.Size = new System.Drawing.Size(75, 23);
+            this.insert.TabIndex = 7;
+            this.insert.Text = "Insert";
+            this.insert.UseVisualStyleBackColor = true;
+            this.insert.Click += new System.EventHandler(this.insert_Click);
+            // 
+            // update
+            // 
+            this.update.Location = new System.Drawing.Point(154, 207);
+            this.update.Name = "update";
+            this.update.Size = new System.Drawing.Size(75, 23);
+            this.update.TabIndex = 8;
+            this.update.Text = "Update";
+            this.update.UseVisualStyleBackColor = true;
+            this.update.Click += new System.EventHandler(this.update_Click);
+            // 
+            // view
+            // 
+            this.view.Location = new System.Drawing.Point(244, 207);
+            this.view.Name = "view";
+            this.view.Size = new System.Drawing.Size(75, 23);
+            this.view.TabIndex = 9;
+            this.view.Text = "View";
+            this.view.UseVisualStyleBackColor = true;
+            this.view.Click += new System.EventHandler(this.view_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 271);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(443, 94);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // delete
+            // 
+            this.delete.Location = new System.Drawing.Point(336, 207);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(75, 23);
+            this.delete.TabIndex = 11;
+            this.delete.Text = "Delete";
+            this.delete.UseVisualStyleBackColor = true;
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // clear
+            // 
+            this.clear.Location = new System.Drawing.Point(386, 153);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(75, 23);
+            this.clear.TabIndex = 12;
+            this.clear.Text = "Clear";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 377);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.view);
+            this.Controls.Add(this.update);
+            this.Controls.Add(this.insert);
+            this.Controls.Add(this.show);
+            this.Controls.Add(this.age);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.id);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox id;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox age;
+        private System.Windows.Forms.Button show;
+        private System.Windows.Forms.Button insert;
+        private System.Windows.Forms.Button update;
+        private System.Windows.Forms.Button view;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.Button clear;
+    }
+}
+
---
+++ Databaseconnectivity/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Databaseconnectivity
+{
+    public partial class Form1 : Form
+    {
+        SqlDataAdapter sda;
+        SqlCommandBuilder scb;
+        DataTable dtb;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void show_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source =.; Initial Catalog = mydb; Integrated Security = True");
+            SqlDataAdapter sda = new SqlDataAdapter("SELECT * FROM mytb where ID='" + id.Text + "'", con);
+            DataTable dtb = new DataTable();
+            sda.Fill(dtb);
+            try
+            {
+                id.Text = dtb.Rows[0][0].ToString();
+                name.Text = dtb.Rows[0][1].ToString();
+                age.Text = dtb.Rows[0][2].ToString();
+            }
+            catch(Exception )
+            {
+
+            }
+           
+
+
+        }
+
+        private void insert_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=mydb;Integrated Security=True");
+            con.Open();
+            try
+            {
+                SqlCommand scom = new SqlCommand("INSERT INTO mytb (id,name,age) VALUES(@id,@name,@age)", con);
+                scom.Parameters.Add("id", SqlDbType.Int);
+                scom.Parameters["id"].Value = id.Text;
+
+                scom.Parameters.Add("name", SqlDbType.VarChar);
+                scom.Parameters["name"].Value = name.Text;
+
+                scom.Parameters.Add("age", SqlDbType.Int);
+                scom.Parameters["age"].Value = age.Text;
+
+
+                scom.ExecuteNonQuery();
+                con.Close();
+
+            }
+            catch (Exception)
+            {
+
+            }
+
+
+        }
+
+        private void view_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=mydb;Integrated Security=True");
+            sda = new SqlDataAdapter(@"SELECT * FROM mytb", con);
+            dtb = new DataTable();
+            sda.Fill(dtb);
+            dataGridView1.DataSource = dtb;
+            MessageBox.Show("The datagrid lists all your data");
+
+        }
+
+        private void update_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=mydb;Integrated Security=True");
+                con.Open();
+
+                SqlCommand scom =
+                                new SqlCommand("UPDATE mytb SET name=@name, age=@age, salary=@salary WHERE id=@id", con);
+                
+                {
+                    scom.Parameters.Add("id", SqlDbType.Int).Value = id.Text;
+
+                    scom.Parameters.Add("name", SqlDbType.VarChar).Value = name.Text;
+
+                    scom.Parameters.Add("age", SqlDbType.Int).Value = age.Text;
+
+
+                    
+                    scom.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+                
+            
+
+                
+
+            }
+
+        private void delete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=mydb;Integrated Security=True");
+
+                con.Open();
+                SqlCommand scom =
+                                new SqlCommand("DELETE FROM mytb WHERE id=@id", con);
+                
+                {
+                    scom.Parameters.Add("id", SqlDbType.Int).Value = id.Text;
+
+                   
+
+
+                    
+                    scom.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("pls fill in the form");
+            }
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                id.Text = string.Empty;
+                name.Text = string.Empty;
+                age.Text = string.Empty;
+                
+            }
+            catch (Exception ex)
+            {
+               
+            }
+        }
+    }
+    }
+
